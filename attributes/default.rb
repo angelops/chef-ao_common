8299b5325d0c94d999f29cf3bcfefbc7457684ed
Justin Ryan <juryan@ziprealty.com>
2015-08-05 19:03:26 -0700

    add tcpdump to package list, bump version.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 node.default['set_fqdn'] = 'aur.test.ziprealty.com'
 node.default['apt']['compile_time_update'] = true
-default['zip_common']['default_packages'] = %w{rdiff-backup tmux iotop htop}
+default['zip_common']['default_packages'] = %w{rdiff-backup tmux iotop htop tcpdump}
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'juryan@ziprealty.com'
 license 'Apache 2.0'
 description 'Installs/Configures zip_common'
 long_description 'Installs/Configures zip_common'
-version '0.1.6'
+version '0.1.7'
 
 depends 'apt'
 depends 'yum-epel'

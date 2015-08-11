c201d5f040b5a89bef3b4cc467288968985b9ddc
Justin Ryan <juryan@ziprealty.com>
2015-08-11 15:13:19 -0700

    add packages.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 node.default['set_fqdn'] = '*.aur.test.ziprealty.com'
 node.default['apt']['compile_time_update'] = true
-default['zip_common']['default_packages'] = %w{rdiff-backup tmux iotop htop tcpdump}
+default['zip_common']['default_packages'] = %w{rdiff-backup tmux iotop htop tcpdump nc util-linux git}

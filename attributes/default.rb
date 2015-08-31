de007ee1eee725f6c21c23ff76e304795f617b4d
Justin Alan Ryan <juryan@ziprealty.com>
2015-08-31 11:25:21 -0700

    no package 'nc' in ubuntu.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 node.default['set_fqdn'] = '*.aur.test.ziprealty.com'
 node.default['apt']['compile_time_update'] = true
 default['zip_common']['default_centos_packages'] = %w{rdiff-backup tmux iotop htop tcpdump util-linux git mosh}
-default['zip_common']['default_ubuntu_packages'] = %w{rdiff-backup tmux iotop htop tcpdump nc util-linux git mosh}
+default['zip_common']['default_ubuntu_packages'] = %w{rdiff-backup tmux iotop htop tcpdump util-linux git mosh}
 default['zip_common']['default_debian_packages'] = %w{rdiff-backup tmux iotop htop tcpdump util-linux git mosh}

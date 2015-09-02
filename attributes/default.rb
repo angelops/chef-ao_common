78e1c909337fcf34d970e48605006524aacd937f
Justin Alan Ryan <juryan@ziprealty.com>
2015-09-02 14:18:08 -0700

    default -> node.default

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 node.default['set_fqdn'] = '*.aur.test.ziprealty.com'
 node.default['apt']['compile_time_update'] = true
-default['zip_common']['default_centos_packages'] = %w{rdiff-backup tmux iotop htop tcpdump util-linux git mosh yum-cron}
-default['zip_common']['default_ubuntu_packages'] = %w{rdiff-backup tmux iotop htop tcpdump util-linux git mosh}
-default['zip_common']['default_debian_packages'] = %w{rdiff-backup tmux iotop htop tcpdump util-linux git mosh}
+node.default['zip_common']['default_centos_packages'] = %w{rdiff-backup tmux iotop htop tcpdump util-linux git mosh yum-cron}
+node.default['zip_common']['default_ubuntu_packages'] = %w{rdiff-backup tmux iotop htop tcpdump util-linux git mosh}
+node.default['zip_common']['default_debian_packages'] = %w{rdiff-backup tmux iotop htop tcpdump util-linux git mosh}

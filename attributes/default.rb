00bce98f2a3f556814af4415c3ab4e35878c191b
Justin Alan Ryan <juryan@ziprealty.com>
2015-09-10 11:38:15 -0700

    add root to sudo users list to see if it needs to become a list, bump version.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 node.default['set_fqdn'] = '*.aur.test.ziprealty.com'
 node.default['apt']['compile_time_update'] = true
-node.default['sudo']['users'] = %w{bootstrap}
+node.default['sudo']['users'] = %w{bootstrap root}
 default['zip_common']['default_centos_packages'] = %w{rdiff-backup tmux iotop htop tcpdump util-linux git mosh yum-cron}
 default['zip_common']['default_ubuntu_packages'] = %w{rdiff-backup tmux iotop htop tcpdump util-linux git mosh}
 default['zip_common']['default_debian_packages'] = %w{rdiff-backup tmux iotop htop tcpdump util-linux git mosh}

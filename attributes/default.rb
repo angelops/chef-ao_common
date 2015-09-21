a19432bfa60f2e8f936470b74f2b07a3e6bc6e23
Justin Alan Ryan <juryan@ziprealty.com>
2015-09-21 13:19:45 -0700

    don't dupe root in sudoers, bump version.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 node.default['set_fqdn'] = '*.aur.test.ziprealty.com'
 node.default['apt']['compile_time_update'] = true
-node.default['authorization']['sudo']['users'] = %w{bootstrap root}
+node.default['authorization']['sudo']['users'] = %w{bootstrap}
 default['zip_common']['default_centos_packages'] = %w{vim rdiff-backup tmux iotop htop tcpdump util-linux git mosh yum-cron}
 default['zip_common']['default_ubuntu_packages'] = %w{vim rdiff-backup tmux iotop htop tcpdump util-linux git mosh}
 default['zip_common']['default_debian_packages'] = %w{vim rdiff-backup tmux iotop htop tcpdump util-linux git mosh}

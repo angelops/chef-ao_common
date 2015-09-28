b3fac463363172e00920e4273b3b95481b2ec428
Justin Alan Ryan <juryan@ziprealty.com>
2015-09-28 12:08:07 -0700

    install curl everywhere.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 node.default['set_fqdn'] = '*.aur.test.ziprealty.com'
 node.default['apt']['compile_time_update'] = true
 node.default['authorization']['sudo']['users'] = %w{bootstrap}
-default['zip_common']['default_centos_packages'] = %w{traceroute vim rdiff-backup tmux iotop htop tcpdump util-linux git mosh yum-cron}
-default['zip_common']['default_ubuntu_packages'] = %w{traceroute runit vim rdiff-backup tmux iotop htop tcpdump util-linux git mosh}
-default['zip_common']['default_debian_packages'] = %w{traceroute runit vim rdiff-backup tmux iotop htop tcpdump util-linux git mosh}
+default['zip_common']['default_centos_packages'] = %w{traceroute curl vim rdiff-backup tmux iotop htop tcpdump util-linux git mosh yum-cron}
+default['zip_common']['default_ubuntu_packages'] = %w{traceroute curl runit vim rdiff-backup tmux iotop htop tcpdump util-linux git mosh}
+default['zip_common']['default_debian_packages'] = %w{traceroute curl runit vim rdiff-backup tmux iotop htop tcpdump util-linux git mosh}
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'juryan@ziprealty.com'
 license 'Apache 2.0'
 description 'Installs/Configures zip_common'
 long_description 'Installs/Configures zip_common'
-version '0.2.14'
+version '0.2.15'
 
 depends 'apt'
 depends 'yum-epel'
